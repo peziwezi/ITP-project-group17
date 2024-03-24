56ed637143a959670a2a1fade73c6f25fa032750
pedaKrk <peter.karkulik@gmail.com>
2024-03-24 01:43:31 +0100

    project

---
+++ Source/ITP/ITPGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ITPGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AITPGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AITPGameMode();
+};
+
+
+
